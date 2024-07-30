85cf4a143bb66dfd06d9a2060bd4578b0d5f24ae
joemcgee4151986 <joerobertmcgee@gmail.com>
2024-07-30 15:14:38 +0900

    using mock api to test mobile app

--- components/Body.js
+++ components/Body.js
@@ -1,19 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import { StyleSheet, Text, View } from 'react-native';
 export default function Body() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Nope</Text>
-    
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        data.map((post) => (
+          <View key={post.id} style={styles.item}>
+            <Text style={styles.title}>{post.title}</Text>
+            <Text>{post.body}</Text>
+          </View>
+        ))
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'green',
+    flex: 2,
+    justifyContent: 'center',
     alignItems: 'center',
-justifyContent: 'center',
+    padding: 15,
+  },
+  item: {
+    backgroundColor: 'yellow',
+    padding: 20,
+    marginVertical: 18,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: 'bold',
   },
 });
