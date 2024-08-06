988254d1d6630026cdad1580d312980c491a20d4
joemcgee4151986 <joerobertmcgee@gmail.com>
2024-08-06 16:46:24 +0900

    used pokemon api as a mockup to see how it's displayed on my mobile app

--- components/Body.js
+++ components/Body.js
@@ -1,31 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 
 export default function Body() {
-  const [data, setData] = useState(null);
+  const [pokemon, setPokemon] = useState(null);
+  
   const [loading, setLoading] = useState(true);
  
   const getRandomPostId = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
   useEffect(() => {
-    const postId =  getRandomPostId(1, 100) 
-    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    const pokemonId =  getRandomPostId(1, 100) 
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
       .then((resp) => resp.json())
-      .then((json) => setData(json))
+      .then((data) => setPokemon(data))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
-
+  const renderType = ({ item }) => (
+    <View style={styles.typeContainer}>
+      <Text style={styles.typeText}>{item.type.name}</Text>
+    </View>
+  );
   return (
     <View style={styles.container}>
       {loading ? (
         <Text>Loading...</Text>
       ) : (
-        data && (
-          <View key={data.id} style={styles.item}>
-            <Text style={styles.title}>{data.title}</Text> 
-            <Text>{data.body}</Text> 
+        pokemon && (
+          <View style={styles.pokemonContainer}>
+            <Text style={styles.pokemonName}>{pokemon.name}</Text>
+            <Image
+              source={{ uri: pokemon.sprites.front_default }}
+              style={styles.pokemonImage}
+            />
+            <FlatList
+              data={pokemon.types}
+              renderItem={renderType}
+              keyExtractor={(item) => item.slot.toString()}
+              style={styles.typeList}
+              
+            />
+            
           </View>
         )
       )}
@@ -38,15 +54,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 15,
+    backgroundColor: 'white'
   },
-  item: {
-    backgroundColor: 'yellow',
-    padding: 20,
-    marginVertical: 18,
-    borderRadius: 10,
+  pokemonContainer: {
+    alignItems: 'center',
   },
-  title: {
-    fontSize: 12,
+  pokemonName: {
+    fontSize: 34,
     fontWeight: 'bold',
+    marginBottom: 16,
+
+  },
+  pokemonImage: {
+    width: 100,
+    height: 100,
+    marginBottom: 16,
+  },
+  typeList: {
+    marginTop: 8,
+  },
+  typeContainer: {
+    backgroundColor: '#eee',
+    padding: 8,
+    marginVertical: 4,
+    borderRadius: 4,
+  },
+  typeText: {
+    fontSize: 18,
   },
 });
